8082e2562b227ff059aae030a85e7d1fd65c5f9c
Metehan <114250887+MetehanSaman@users.noreply.github.com>
2023-04-21 01:06:05 +0300

    .

---
+++ StarPyramid.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int base, i, m, b, a, variable;
+	variable = 0;
+	cout << "Enter the base number of stars of pyramid: ";
+	cin >> base;
+
+	for (a = 1; a <= base; a += 2) {
+
+
+		for (i = 1; i <= (base - a)  / 2; i++) {
+			cout << " ";
+			variable++;
+		}
+		for (m = 1; m <= a; m++) {
+			cout << "*";
+		}
+		for (b = 1; b <= variable; b++) {
+			cout << " ";
+		}
+		cout << endl;
+
+	}
+	
+	
+	
+
+
+
+
+
+
+
+
+
+}
